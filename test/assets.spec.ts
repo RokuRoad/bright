04ac8862ba44d7f8ce6420b152b77d29ecc2d88f
Igor Alpert <ialpert-c@scrippsnetworks.com>
2018-06-17 12:47:50 -0400

    fix(parser): Release 1.x

--- test/assets.spec.ts
+++ test/assets.spec.ts
@@ -1,7 +1,7 @@
 import { basename } from 'path'
 import { files, scanFile } from './helpers'
 
-describe.only('Asset tests', () => {
+describe('Asset tests', () => {
   files(__dirname + '/assets/**/*.brs').forEach((path: string) => {
     test(basename(path, '.brs'), () => {
       expect(scanFile(path)).toBeTruthy()
